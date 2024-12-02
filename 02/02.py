6c6af69f0838b3cb975d78cbdb8eac6345c00550
petrlos <petrlos@gmail.com>
2024-12-02 14:17:06 +0100

    Day 2: part1+part2 commented

--- 02/02.py
+++ 02/02.py
@@ -9,20 +9,36 @@ def parse_input(lines):
     return numbers
 
 def safe_part1(numbers):
+    #all numbers must be in order + max difference of 3
     if numbers == sorted(numbers, reverse=True) or numbers == sorted(numbers):
         for i in range(len(numbers)-1):
             if abs(numbers[i] - numbers[i+1]) > 3 or numbers[i] == numbers[i+1]:
                 return False
         return True
 
+def safe_part2(numbers):
+    #leave out one number from list and check with part1 function
+    for i in range(len(numbers)):
+        one_out = numbers[:i] + numbers[i+1:]
+        if safe_part1(one_out):
+            return True
+    return False
+
 #MAIN
 with open("data.txt") as file:
     lines = file.read().splitlines()
 
-numbs = parse_input(lines)
+data = parse_input(lines)
 
-counter = 0
-for num in numbs:
-    if safe_part1(num):
-        counter += 1
-print("Part 1:", counter)
+counter_part1 = 0
+counter_part2 = 0
+for nums in data:
+    if safe_part1(nums):
+        counter_part1 += 1
+    else:
+        #not safe for part1? check part2
+        if safe_part2(nums):
+            counter_part2 += 1
+
+print("Part 1:", counter_part1)
+print("Part 2:", counter_part1+counter_part2)
