732749d2ccee314e1ae73e4f93e0c792dd382b43
petrlos <petrlos@gmail.com>
2024-12-04 13:51:39 +0100

    Day 4: part1+part2 commented

--- 04/04.py
+++ 04/04.py
@@ -13,6 +13,20 @@ def find_XMAS(coord, direction):
             return False
     return True
 
+def find_cross_MAS(coord):
+    ok_counter = 0
+    r, c = coord
+    corners_top = [(-1 + r,-1 + c), (-1 + r, 1 + c)] #topleft, topright corner
+    corners_bot = [( 1 + r, 1 + c), ( 1 + r,-1 + c)] #bottomright,
+    for cor_top, cor_bot in zip(corners_top, corners_bot):
+        if cor_top not in grid.keys() or cor_bot not in grid.keys():
+            #out of grid
+            return False
+        if grid[cor_top] != grid[cor_bot] and grid[cor_top] not in "XA" and grid[cor_bot] not in  "XA":
+            #if they are not the same and not "X" or "A"
+            ok_counter += 1
+    return ok_counter == 2 #both lines of the cross must be correct
+
 #MAIN
 with open("data.txt") as file:
     lines = file.read().splitlines()
@@ -24,11 +38,17 @@ for r, line in enumerate(lines):
 
 directions = {(dr, dc) for dr in [-1, 0, 1] for dc in [-1, 0, 1] if (dr, dc) != (0, 0)}
 
-counter = 0
+#Part1
+counter_1 = 0
+counter_2 = 0
 for coord, char in grid.items():
-    if char == "X":
-        for direction in directions:
+    if char == "X": #checker for part1
+        for direction in directions: #check every direction starting from "X"
             if find_XMAS(coord, direction):
-                counter += 1
+                counter_1 += 1
+    if char == "A": #checker for part2
+        if find_cross_MAS(coord):
+            counter_2 += 1
 
-print(counter)
+print("Part 1:", counter_1)
+print("Part 2:", counter_2)
