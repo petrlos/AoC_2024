9247ca518a2069c6596f747e0fc10b67fee5b0da
petrlos <petrlos@gmail.com>
2024-12-09 17:43:50 +0100

    Part 1

--- 09/09.py
+++ 09/09.py
@@ -4,35 +4,31 @@ from icecream import ic
 def get_checksum(result):
     return sum(id * int(char) for id, char in enumerate(result))
 
-def control_sum(file_system):
-    file_system = file_system.replace(".", "")
-    return sum(int(char) for char in file_system)
+def compress_single_slots(line):
+    file_system = list()
+    for id, char in enumerate(line):
+        if id % 2 == 0:
+            file_system.extend([str(id // 2)] * int(char))
+        else:
+            file_system += ["."] * int(char)
+
+    compressed_file_system = list()
+    id = 0
+    while len(compressed_file_system) < len(file_system):
+        char = file_system[id]
+        if char == ".":
+            if file_system[-1] == ".":
+                char = "."
+            else:
+                char = file_system[-1]
+            file_system.pop(-1)
+        if char != ".":
+            compressed_file_system.append(char)
+            id += 1
+    return get_checksum(compressed_file_system)
 
 #MAIN
 with open("data.txt") as file:
     line = file.read()
 
-
-file_system = list()
-for id, char in enumerate(line):
-    if id % 2 == 0:
-        file_system.extend([str(id // 2)]*int(char))
-    else:
-        file_system += ["."]*int(char)
-
-
-compressed_file_system = list()
-id = 0
-while len(compressed_file_system) < len(file_system):
-    char = file_system[id]
-    if char == ".":
-        if file_system[-1] == ".":
-            char = "."
-        else:
-            char = file_system[-1]
-        file_system = file_system[:-1]
-    if char != ".":
-        compressed_file_system.append(char)
-        id += 1
-
-ic(get_checksum(compressed_file_system))
+print("Part 1:", compress_single_slots(line))
