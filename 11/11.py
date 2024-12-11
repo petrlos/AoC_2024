c1a909b12407808997240e137999464e3ab2b17d
petrlos <petrlos@gmail.com>
2024-12-11 19:38:01 +0100

    Part 1 + Part 2

--- 11/11.py
+++ 11/11.py
@@ -1,6 +1,8 @@
 #Advent of Code 2024: Day 11
 from icecream import ic
 from collections import defaultdict
+from datetime import datetime
+time_start = datetime.now()
 
 def brute_blink(stones):
     new_stones = []
@@ -15,27 +17,29 @@ def brute_blink(stones):
             new_stones.append(str(int(stone)*2024))
     return new_stones
 
-def blink(stones):
-    new_stones = defaultdict(int)
-    for stone, count in stones.items():
-        news = solve_single_stone(stone)
-        for new in news:
-            new_stones[new] += count
-    return new_stones
+def blink(repeats, line):
+    stones = defaultdict(int)
+    for num in line.split(" "):
+        stones[num] += 1 #basic setup
+    for i in range(repeats):
+        new_stones = defaultdict(int)
+        for stone, count in stones.items():
+            if stone == "0":
+                stones["0"] = 0 #all stones with this number are consumed
+                new_stones["1"] += count #and create a new stone(stones eventually)
+            elif len(stone) % 2 == 0:
+                l = len(stone)
+                left, right = stone[:l // 2], stone[l // 2:]
+                stones[stone] = 0
+                new_stones[left] += count
+                new_stones[str(int(right))] += count
+            else:
+                stones[stone] = 0
+                new_stones[str(int(stone) * 2024)] += count
+        stones.update(new_stones)
+    return sum(stones.values())
 
-def solve_single_stone(stone):
-    result = []
-    if stone == "0":
-        result.append("1")
-    elif len(stone) % 2 == 0:
-        l = len(stone)
-        left, right = stone[:l // 2], stone[l // 2:]
-        result += [left] + [str(int(right))]
-    else:
-        result.append(str(int(stone) * 2024))
-    return result
-
-
+#MAIN
 with open("data.txt") as file:
     line = file.read()
 
@@ -43,16 +47,11 @@ stones = line.split(" ")
 
 for i in range(25):
     stones = brute_blink(stones)
-print(len(stones))
+print("Part 1 brute force:", len(stones))
+print("Runtime: ", datetime.now() - time_start)
+print(" ")
 
-stones = defaultdict(int)
-for num in line.split(" "):
-    stones[num] += 1
-
-for i in range(75):
-    ic(i, len(stones))
-    for number, count in stones.items():
-        new_stones = blink(stones)
-    stones = new_stones
-
-ic(sum(stones.values()))
+for id, repeat in enumerate([25,75]):
+    time_start = datetime.now()
+    print(f"Part {id+1}:", blink(repeat, line))
+    print("Runtime:", datetime.now() - time_start)
