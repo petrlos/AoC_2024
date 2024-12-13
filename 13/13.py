29023329b122f7a0a1d2de13b86bf274096351d9
petrlos <petrlos@gmail.com>
2024-12-13 18:44:37 +0100

    Day 13: Part 1 + Part2

--- 13/13.py
+++ 13/13.py
@@ -1,18 +1,21 @@
 #Advent of Code 2024: Day 13
 import re
-from icecream import ic
 from sympy import symbols, Eq, solve
 
-def check_block(block):
+def check_block(block, part):
     a, d, b, e, c, f = list(map(int, re.findall(r"\d+", block)))
     x, y = symbols('x y')
+    c += part
+    f += part
     eq1 = Eq(a * x + b * y, c)
     eq2 = Eq(d * x + e * y, f)
     solution = solve((eq1, eq2), (x, y))
     results = [solution[x], solution[y]]
     result_ok = True
     for result in results:
-        if result > 100 or int(result) != result:
+        if int(result) != result:
+            result_ok = False
+        if part == 0 and result > 100:
             result_ok = False
     if result_ok:
         tokens = int(results[0]*3 + results[1])
@@ -25,9 +28,8 @@ with open("data.txt") as file:
     blocks = file.read().split("\n\n")
 
 tokens_needed = 0
-for block in blocks:
-    tokens_needed += check_block(block)
-
-print("Part 1:", tokens_needed)
-
-
+for id, i in enumerate([0, 10000000000000]):
+    tokens_needed = 0
+    for block in blocks:
+        tokens_needed += check_block(block, i)
+    print(f"Part {id+1}: {tokens_needed}")
