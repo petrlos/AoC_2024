eead998d5aeb6080a7d90a496f76208947cd9837
petrlos <petrlos@gmail.com>
2024-12-14 21:14:07 +0100

    Day 14: Part 1 + Part2

--- 14/14.py
+++ 14/14.py
@@ -1,28 +1,27 @@
 #Advent of Code 2024: Day 14
 import re
-from icecream import ic
 from math import prod
 from collections import Counter, defaultdict
-from dataclasses import dataclass
 
-def print_grid(robots):
-    c_robots = Counter(robots)
-    for row in range(r_max):
-        for col in range(c_max):
+def print_grid(robots, r_max, c_max):
+    for row in range(44, r_max-24): #constraints tested manually, so that output looks nice for my data
+        for col in range(30, c_max-10):
             if (row, col) in robots:
-                print(c_robots[(r,c)], end="")
+                print("*", end="")
             else:
                 print(".", end="")
         print(" ")
     print(" ")
 
-def move_part1(lines, r_max, c_max):
+def move_part1(lines, r_max, c_max, repeats):
     sectors = defaultdict(int)
+    robots = set()
     for line in lines:
         c, r, dc, dr = list(map(int, (re.findall(r"-?\d+", line))))
-        for i in range(100):
+        for i in range(1,repeats):
             r = (r + dr) % r_max
             c = (c + dc) % c_max
+        robots.add((r,c))
         r = r - r_max // 2 #correct row by 1/2 size of a grid upwards
         c = c - c_max // 2 #correct col by 1/2 size of a grid left
         if c == 0 or r == 0: #lies in the middle
@@ -30,14 +29,17 @@ def move_part1(lines, r_max, c_max):
         c = c // abs(c) #leave only sign: +1 or -1
         r = r // abs(r)
         sectors[(r,c)] += 1
+    if repeats == 7372:
+        print("Part 2 ", i)
+        print_grid(robots, r_max, c_max)
     return prod(sectors.values())
 
 #MAiN
-
-with open("test.txt")as file:
-    lines = file.read().splitlines()
-print(move_part1(lines, 7,11))
-
 with open("data.txt")as file:
     lines = file.read().splitlines()
-print(move_part1(lines, 103,101))
+print("Part 1:",move_part1(lines, 103,101, repeats=101)) #101 because cycle starts at 1
+
+#after 58 iterations appears a horizontal stripe every 103 more steps
+#tested manually from 5000 iterations to find a tree
+#7371 is a correct answer for my data
+move_part1(lines, 103,101, repeats=7372)
