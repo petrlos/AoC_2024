5df2cf734d2b89b9562e6ba843b73869f59e3905
petrlos <petrlos@gmail.com>
2025-01-19 14:37:55 +0100

    Day 20: Part 1 - much faster

---
+++ 20/20_2.py
@@ -0,0 +1,42 @@
+#Advent of Code 2024: Day 20
+from collections import deque
+
+def check_shortcut(pair):
+    c1, c2 = pair
+    if c1 not in distances.keys() or c2 not in distances.keys():
+        return -1
+    return abs(distances[c1] - distances[c2])
+
+#MAIN
+with open("data.txt") as file:
+    grid = file.read().splitlines()
+
+for r in range(len(grid)):
+    for c in range(len(grid[0])):
+        if grid[r][c] == "S":
+            start = (r, c)
+
+queue = deque([start])
+max_r = len(grid)
+max_c = len(grid[0])
+distances = {start: 0}
+
+while queue:
+    r, c = queue.popleft()
+    for new_r, new_c in [(r+1, c), (r-1, c), (r, c+1), (r, c-1)]:
+        if new_r not in range(max_r) or new_c not in range(max_c): continue
+        if grid[new_r][new_c] == "#": continue
+        if (new_r, new_c) in distances.keys(): continue
+        queue.append((new_r, new_c))
+        distances[(new_r, new_c)] = distances[(r, c)] + 1
+
+result = 0
+for r in range(max_r):
+    for c in range(max_c):
+        if grid[r][c] != "#": continue
+        oposite_coords = [[(r + 1, c), (r - 1, c)], [(r, c + 1), (r, c - 1)]]
+        for pair in oposite_coords:
+            shortcut = check_shortcut(pair)
+            if shortcut > 100:
+                result += 1
+print("Part 1:", result)
